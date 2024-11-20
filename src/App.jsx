1cd4cf357634742119ab201884c990fbb6b64c69
DeclanGH <declanoonunkwo@gmail.com>
2024-11-20 12:10:41 -0500

    add vercel analytics, fix homepage nav

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,30 @@ import InviteHandler from "./components/helpers/InviteHandler.jsx";
 import GroupPageNavigator from "./components/pages/GroupPageNavigator.jsx";
 import BacklogPage from "./components/pages/BacklogPage.jsx";
 import SprintPage from "./components/pages/SprintPage.jsx";
+import {Analytics} from "@vercel/analytics/vue";
 
 function App() {
 
     useUserUpdate();
 
     return (
-        <Router>
-            <Routes>
-                <Route path={ROUTE.HOME} element={<Home/>} />
+        <div>
+            <Router>
+                <Routes>
+                    <Route path={ROUTE.HOME} element={<Home/>} />
 
-                <Route element={<PrivateRoute />}>
-                    <Route path={`${ROUTE.DASHBOARD}`} element={<Dashboard/>} />
-                    <Route path={`${ROUTE.PROJECT}${PROJECT_ID}`} element={<ProjectPage />} />
-                    <Route path={`${ROUTE.INVITE}${PROJECT_ID}`} element={<InviteHandler />} />
-                    <Route path={`${ROUTE.PROJECT}${PROJECT_ID}${ROUTE.GROUP}${GROUP_ID}`} element={<GroupPageNavigator />} />
-                    <Route path={`${ROUTE.PROJECT}${PROJECT_ID}${ROUTE.GROUP}${GROUP_ID}${ROUTE.BACKLOG}${BACKLOG_ID}`} element={<BacklogPage />} />
-                    <Route path={`${ROUTE.PROJECT}${PROJECT_ID}${ROUTE.GROUP}${GROUP_ID}${ROUTE.SPRINT}${SPRINT_ID}`} element={<SprintPage />} />
-                </Route>
-            </Routes>
-        </Router>
+                    <Route element={<PrivateRoute />}>
+                        <Route path={`${ROUTE.DASHBOARD}`} element={<Dashboard/>} />
+                        <Route path={`${ROUTE.PROJECT}${PROJECT_ID}`} element={<ProjectPage />} />
+                        <Route path={`${ROUTE.INVITE}${PROJECT_ID}`} element={<InviteHandler />} />
+                        <Route path={`${ROUTE.PROJECT}${PROJECT_ID}${ROUTE.GROUP}${GROUP_ID}`} element={<GroupPageNavigator />} />
+                        <Route path={`${ROUTE.PROJECT}${PROJECT_ID}${ROUTE.GROUP}${GROUP_ID}${ROUTE.BACKLOG}${BACKLOG_ID}`} element={<BacklogPage />} />
+                        <Route path={`${ROUTE.PROJECT}${PROJECT_ID}${ROUTE.GROUP}${GROUP_ID}${ROUTE.SPRINT}${SPRINT_ID}`} element={<SprintPage />} />
+                    </Route>
+                </Routes>
+            </Router>
+            <Analytics />
+        </div>
     );
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,20 +1,32 @@
 import {useAuth0} from "@auth0/auth0-react";
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Nav, Navbar, Offcanvas} from "react-bootstrap";
 import LogoutButton from "../buttons/LogoutButton.jsx";
 import LoginButton from "../buttons/LoginButton.jsx";
-import {ROUTE} from "../../common/constants.js";
+import {APPLICATION_NAME, ROUTE} from "../../common/constants.js";
 
 function Home() {
     const { isAuthenticated } = useAuth0();
 
     return(
-        <Navbar bg="dark" data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand href={ROUTE.HOME}>Project Manager</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href={ROUTE.HOME}>About</Nav.Link>
-                </Nav>
-                {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+        <Navbar expand={"lg"} className="navbar-dark bg-dark mb-3">
+            <Container fluid={true}>
+                <Navbar.Brand href={ROUTE.HOME}>{APPLICATION_NAME}</Navbar.Brand>
+                <Navbar.Toggle aria-controls={"offcanvasNavbar-expand-lg"} />
+                <Navbar.Offcanvas
+                    aria-labelledby="offcanvasNavbarLabel-expand-lg"
+                    placement="end"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>{APPLICATION_NAME}</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Nav.Link href={ROUTE.HOME}>About</Nav.Link>
+                            <Nav.Link href={ROUTE.DASHBOARD}>Dashboard</Nav.Link>
+                        </Nav>
+                        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
         </Navbar>
     )
